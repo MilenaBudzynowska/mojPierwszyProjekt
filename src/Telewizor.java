341c215930c5a23b87ff13a50ab6ab1ef32c7927
Damian <damian.budzynowski@gmail.com>
2020-01-20 19:20:39 +0100

    Add Telewizor class

---
+++ src/Telewizor.java
@@ -0,0 +1,9 @@
+public class Telewizor {
+    String marka;
+    int rozmiar;
+
+    public Telewizor(String marka, int rozmiar) {
+        this.marka = marka;
+        this.rozmiar = rozmiar;
+    }
+}
